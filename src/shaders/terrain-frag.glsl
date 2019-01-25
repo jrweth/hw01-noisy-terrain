f87795397e5d38b80b9a3b2b76aca445e0c1581b
J. Reuben Wetherbee <wetherbe@sas.upenn.edu>
2019-01-24 21:38:31 -0500

    fbm noise added

--- src/shaders/terrain-frag.glsl
+++ src/shaders/terrain-frag.glsl
@@ -7,7 +7,7 @@ in vec3 fs_Pos;
 in vec4 fs_Nor;
 in vec4 fs_Col;
 
-in float fs_Sine;
+in float fs_Height;
 
 out vec4 out_Col; // This is the final output color that you will see on your
                   // screen for the pixel that is currently being processed.
@@ -15,5 +15,9 @@ out vec4 out_Col; // This is the final output color that you will see on your
 void main()
 {
     float t = clamp(smoothstep(40.0, 50.0, length(fs_Pos)), 0.0, 1.0); // Distance fog
-    out_Col = vec4(mix(vec3(0.5 * (fs_Sine + 1.0)), vec3(164.0 / 255.0, 233.0 / 255.0, 1.0), t), 1.0);
+    vec3 backgroundColor = vec3(164.0 / 255.0, 233.0 / 255.0, 1.0);
+    vec3 heightColor = vec3(fs_Height * 5.0);
+    vec3 oceanColor = vec3(0.0, 0.0, 1.0);
+    if(fs_Height <= 0.06) heightColor = oceanColor;
+    out_Col = vec4(mix(heightColor, backgroundColor, t), 1.0);
 }
--- src/shaders/terrain-vert.glsl
+++ src/shaders/terrain-vert.glsl
@@ -14,7 +14,7 @@ out vec3 fs_Pos;
 out vec4 fs_Nor;
 out vec4 fs_Col;
 
-out float fs_Sine;
+out float fs_Height;
 
 float random1( vec2 p , vec2 seed) {
   return fract(sin(dot(p + seed, vec2(127.1, 311.7))) * 43758.5453);
@@ -28,11 +28,49 @@ vec2 random2( vec2 p , vec2 seed) {
   return fract(sin(vec2(dot(p + seed, vec2(311.7, 127.1)), dot(p + seed, vec2(269.5, 183.3)))) * 85734.3545);
 }
 
+
+float interpNoiseRandom2to1(vec2 p) {
+    float fractX = fract(p.x);
+    float x1 = p.x - fractX;
+    float x2 = x1 + 1.0;
+    float fractY = fract(p.y);
+    float y1 = p.y - fractY;
+    float y2 = y1 + 1.0;
+
+    float v1 = random1(vec2(x1, y1), vec2(12.232, 3212.123));
+    float v2 = random1(vec2(x2, y1), vec2(12.232, 3212.123));
+    float v3 = random1(vec2(x1, y2), vec2(12.232, 3212.123));
+    float v4 = random1(vec2(x2, y2), vec2(12.232, 3212.123));
+
+    float i1 = smoothstep(v1, v2, fractX);
+    float i2 = smoothstep(v3, v4, fractX);
+
+    return smoothstep(i1, i2, fractY);
+
+}
+
+float fbm2to1(vec2 p) {
+    float total  = 0.0;
+    float persistence = 0.5;
+    float octaves = 8.0;
+
+    for(float i = 0.0; i < octaves; i++) {
+        float freq = pow(2.0, i);
+        float amp = pow(persistence, i);
+        total = total + interpNoiseRandom2to1(vec2(p.x * freq, p.y * freq)) * amp;
+    }
+    return total;
+}
+
 void main()
 {
+  float sampling = 1.0;
   fs_Pos = vs_Pos.xyz;
-  fs_Sine = (sin((vs_Pos.x + u_PlanePos.x) * 3.14159 * 0.1) + cos((vs_Pos.z + u_PlanePos.y) * 3.14159 * 0.1));
-  vec4 modelposition = vec4(vs_Pos.x, fs_Sine * 2.0, vs_Pos.z, 1.0);
+  fs_Height = (sin((vs_Pos.x + u_PlanePos.x) * 3.14159 * 0.1) + cos((vs_Pos.z + u_PlanePos.y) * 3.14159 * 0.1));
+  fs_Height = fbm2to1(vec2(vs_Pos.x + u_PlanePos.x, vs_Pos.z + u_PlanePos.y) * sampling) / 10.0;
+  if(fs_Height < 0.05) fs_Height = 0.05;
+
+  vec4 modelposition = vec4(vs_Pos.x, fs_Height * 2.0, vs_Pos.z, 1.0);
   modelposition = u_Model * modelposition;
   gl_Position = u_ViewProj * modelposition;
 }
