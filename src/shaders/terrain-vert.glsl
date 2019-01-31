6b32ccd92ae1c8845e200c595fa8ded461c85863
J. Reuben Wetherbee <wetherbe@sas.upenn.edu>
2019-01-31 08:18:36 -0500

    misc

--- src/shaders/terrain-vert.glsl
+++ src/shaders/terrain-vert.glsl
@@ -305,7 +305,7 @@ void main()
 
   fs_Pos = vs_Pos.xyz;
   fs_Biome = calcBiome(worldPlanePos);
-  fs_Biome = vec3(0.0, 1.0, 1.0);
+  //fs_Biome = vec3(0.0, 1.0, 1.0);
   fs_Height = calcHeight(worldPlanePos, fs_Biome);
   fs_Nor = calcNormal(worldPlanePos, fs_Biome);
   fs_Col = vs_Col;
